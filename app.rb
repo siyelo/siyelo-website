5cfffea61609f87c6a14a5a6b39d3e66017b0105
Ile Eftimov <ileeftimov@gmail.com>
2013-12-24 03:01:00 +0100

    refactor the email sender/validator

--- app.rb
+++ app.rb
@@ -30,36 +30,12 @@ class SinatraBootstrap < Sinatra::Base
   end
 
   post '/hireus' do
-    full_name = request.params["full_name"]
-    email = request.params["email"]
-    phone = request.params["phone"]
-    description = request.params["description"]
-    budget = request.params["budget"]
-
-    unless EmailValidator.validate(request.params)
+    unless EmailValidator.validate!(request.params)
       flash[:error] = 'Your email form is invalid!'
-      redirect '/'
+      redirect '/hireus'
     end
 
-    mail_body = %Q(
-    Hey Glenn!
-
-    We got contacted by #{full_name}.
-    They'd describe their project as:
-    #{description}
-
-    Their budget is #{budget}.
-    You can contact them by phone on #{phone}.
-    Or by email on #{email}.
-
-    Cheers!)
-
-    Mail.deliver do
-      from "#{email}"
-      to "ile@siyelo.com"
-      subject "Someone wants to hire us!"
-      body mail_body
-    end
+    EmailSender.deliver!(request.params)
 
     flash[:notice] = 'Your email has been sent!'
     redirect '/'
@@ -73,6 +49,38 @@ class EmailValidator
   class << self
     def validate(params)
       ! params.values.any?{|item| item.blank? }
+    end
+  end
+end
+
+class EmailSender
+  class << self
+    def deliver!(params)
+      full_name = params["full_name"]
+      email = params["email"]
+      phone = params["phone"]
+      description = params["description"]
+      budget = params["budget"]
+
+      mail_body = %Q(
+      Hey Glenn!
+
+      We got contacted by #{full_name}.
+      They'd describe their project as:
+      #{description}
+
+      Their budget is #{budget}.
+      You can contact them by phone on #{phone}.
+      Or by email on #{email}.
+
+      Cheers!)
+
+      Mail.deliver do
+        from "#{email}"
+        to "ile@siyelo.com"
+        subject "Someone wants to hire us!"
+        body mail_body
+      end
     end
   end
 end
