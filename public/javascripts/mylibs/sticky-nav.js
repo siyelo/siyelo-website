3b63f7a0b4465c3a6e2d703f383a2b53c07867c0
Ile Eftimov <ileeftimov@gmail.com>
2014-01-15 16:01:23 +0100

    Fix FF sticky nav bug (Thanks STU!)

--- public/javascripts/mylibs/sticky-nav.js
+++ public/javascripts/mylibs/sticky-nav.js
@@ -1,12 +1,16 @@
 function stickyNav(element) {
   var mainNav    = $(element);
   var topOffset = mainNav.offset().top;
-  $(window).scroll(function() {
+
+  setupStickyNav = function() {
     var scrolled  = $(window).scrollTop();
     if(scrolled > topOffset) {
        mainNav.addClass('sticky');
     }else{
        mainNav.removeClass('sticky');
     }
-  });
+  }
+
+  $(window).scroll(setupStickyNav);
+  $(document).ready(setupStickyNav);
 }
