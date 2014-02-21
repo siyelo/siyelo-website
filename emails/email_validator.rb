eb86bed413592418c541c6cc8a43b78418584a0f
Ile Eftimov <ileeftimov@gmail.com>
2014-02-21 15:12:49 +0100

    add custom reverse captcha

--- emails/email_validator.rb
+++ emails/email_validator.rb
@@ -1,7 +1,13 @@
 class EmailValidator
   class << self
     def validate(params)
-      ! params.values.any?{|item| item.blank? }
+      puts params.inspect
+      if params['favourite_movie'].blank?
+        params.delete('favourite_movie')
+        ! params.values.any?{|item| item.blank? }
+      else
+        false
+      end
     end
   end
 end
