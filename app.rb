1391ee60486ff3edf076040e0fbe15eb12e73163
Dime Pashoski <dime@pashoski.com>
2014-08-20 15:48:16 +0200

    seo, titles, tags, verification

--- app.rb
+++ app.rb
@@ -30,38 +30,56 @@ class SinatraBootstrap < Sinatra::Base
   end
 
   get '/' do
+    @title = "We turn great ideas into great applications"
+    @description = "Build your next web/mobile app with an experienced team."
     haml :index
   end
 
   get '/hire-us' do
+    @title = "Hire us for your next project"
+    @description = "Our team of designers and developers can turn your idea into an amazing product"
     haml :hireus, layout: :page
   end
 
   get '/jobs' do
+    @title = "Apply for a job"
+    @description = "If you're enthusiastic about technology and business, drop us a line."
     haml :jobs, layout: :page
   end
 
   get '/internships/full-stack' do
+    @title = "Learn what it takes to be a web developer, apply for internship"
+    @description = "Get into Ruby on Rails, databases, and APIs. Learn frontend tools including HTML5, CSS3, JavaScript, and JQuery. Build interesting web applications"
     haml :fullstack, layout: :page
   end
 
   get '/clients/internet-solutions' do
+    @title = "Internet Solutions case study"
+    @description = "Need to save costs? Rethinking the way you are doing things? We've helped clients save costs by rapidly replacing their legacy systems."
     haml :is, layout: :page
   end
 
   get '/clients/talented-africa' do
+    @title = "Talented Africa case study"
+    @description = "Looking to bring a new web app to market? You need a team to help design, develop and deliver it."
     haml :tad, layout: :page
   end
 
   get '/clients/clinton-health-access-initiative' do
+    @title = "Clinton Health Access Initiative case study"
+    @description = "Apps nowadays must be intuitive to use and visually appealing. We can help design and implement responsive and intuitive interfaces."
     haml :chai, layout: :page
   end
 
   get '/clients/cooco' do
-    haml :eos, layout: :page
+    @title = "Cooco case study"
+    @description = "Software projects (and some developers!) are notoriously difficult to manage. But it doesn't have to be that way. Our collaborative approach helped some of our clients to start fresh and really turn things around."
+    haml :eos, layout: :page    
   end
 
   get '/clients/mad-mimi' do
+    @title = "Mad Mimi case study"
+    @description = "Need to launch new features sooner than your current capacity allows? As an experienced team available at reasonably short notice, we've helped many clients solve this problem."
     haml :mm, layout: :page
   end
 
